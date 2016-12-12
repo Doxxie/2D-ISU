65fe30de31f632ed8593188466bfd56a9608375c
Doxxie <jpatel1999@hotmail.ca>
2016-12-12 08:17:01 -0500

    jay contrib

---
+++ 2D ISU/Assets/Manager.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Manager : MonoBehaviour {
+
+	public void LoadGame(string name){
+        Application.LoadLevel(name);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
